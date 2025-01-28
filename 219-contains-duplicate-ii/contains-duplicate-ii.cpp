82e5df415eba490c85890fbb4ee4e7857fac81b6
Advik Manda <156420092+Manda-Advik@users.noreply.github.com>
2025-01-28 10:00:58 +0530

    Time: 53 ms (68.51%) | Memory: 81.1 MB (67.03%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> lastseen;
+
+        for(int i=0;i<nums.size();i++){
+            if(lastseen.find(nums[i])==lastseen.end()){
+                lastseen[nums[i]]=i;
+            }
+            else{
+                if(i-lastseen[nums[i]]<=k){
+                    return true;
+                }
+            }
+            lastseen[nums[i]] = i;
+        }
+        return false;
+    }
+};
